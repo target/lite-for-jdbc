11066de3163718b4d7faedef283f70d54751012f
MitchellSansSouci <mitchell.sanssouci@target.com>
2023-07-20 12:08:51 -0500

    Review comments.

--- src/test/kotlin/com/target/liteforjdbc/postgres/PostgresDataSourceBuilderTest.kt
+++ src/test/kotlin/com/target/liteforjdbc/postgres/PostgresDataSourceBuilderTest.kt
@@ -139,37 +139,15 @@ class PostgresDataSourceBuilderTest {
     fun `Test targetServerTypes`(targetServerType: TargetServerType) {
         val config = DbConfig(
             databaseName = "dbName",
-            host = "host",
-            port = 1234,
             username = "user",
             password = "password",
-            ssl = false,
-            connectionTimeoutMillis = 1000,
-            idleTimeoutMillis = 2000,
-            keepAliveTime = 3000,
-            maxLifetime = 4000,
-            minimumIdle = 1,
-            maximumPoolSize = 10,
             targetServerType = targetServerType
         )
 
         val result = buildPostgresDataSource(config)
 
-        result.jdbcUrl shouldBe "jdbc:postgresql://host:1234/dbName"
-        result.connectionTimeout shouldBe 1000
-        result.idleTimeout shouldBe 2000
-        result.keepaliveTime shouldBe 3000
-        result.maxLifetime shouldBe 4000
-        result.minimumIdle shouldBe 1
-        result.maximumPoolSize shouldBe 10
-        result.dataSourceProperties["reWriteBatchedInserts"] shouldBe "true"
+        result.dataSourceProperties["targetServerType"] shouldBe targetServerType.jdbcParameterValue
 
-        result.dataSourceProperties.keys shouldNotContain "ssl"
-        result.dataSourceProperties.keys shouldNotContain "sslfactory"
-        result.dataSourceProperties.keys shouldNotContain "sslmode"
-
-        result.username shouldBe "user"
-        result.password shouldBe "password"
     }
 
 }
